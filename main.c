dbc9b8ecfaeaede9a6e86de7266401aff535a7a6
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-15 10:42:55 +0000

    added code
    added code

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main(void) {
   char name[] ={'P','R','I','N','C','E','\0'};
   char name2[] = "PRINCE";
 
-  
+  // String Formate Specifier
+  char name3[50];
+  printf("Enter your name :");
+  scanf("%s",name3);
+
+  printf("Your name is :%s",name3);
   return 0;
 }
