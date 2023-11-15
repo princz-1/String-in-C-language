277053c45d00859ce002e584aba0b9443ed0c7b8
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-15 10:09:28 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@String-in-C-language/main.c
@@ -0,0 +1,13 @@
+// A character array terminated by a "\0"(null character)
+// Null character denoted string termination.
+
+#include <stdio.h>
+
+int main(void) {
+// Example Initialising String
+  char name[] ={'P','R','I','N','C','E','\0'};
+  char name2[] = "PRINCE";
+
+  
+  return 0;
+}
